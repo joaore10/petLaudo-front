06da135c226e3480add28c7af26a4c6524c0daee
joaore10 <joaore1000@gmail.com>
2023-09-25 22:50:53 -0300

    componente chamado read finalizado

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
+
 
 @Component({
   selector: 'app-chamado-read',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoReadComponent implements OnInit {
 
-  constructor() { }
+  chamado: Chamado = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    tecnico: '',
+    cliente: '',
+    nomeCliente: '',
+    nomeTecnico: ''
+  }
+
+  constructor(  private chamadoService: ChamadoService, private toastService: ToastrService,
+   private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void{
+    this.chamadoService.findById(this.chamado.id).subscribe(res => {
+      this.chamado = res;
+      this.chamado.prioridade = this.chamado.prioridade.toString();
+      this.chamado.status = this.chamado.status.toString();
+    }, ex =>{
+      this.toastService.error(ex.error.error);
+    })
   }
 
 }
