c5d1e0450b4a4d73012e22fb560583782f15f734
joaore10 <joaore1000@gmail.com>
2023-10-14 17:09:29 -0300

    Primeiro commit do projeto

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : 'https://helplaudos-prod-7f97cdd7406c.herokuapp.com'
-    // baseUrl : 'http://localhost:8080'
+    //baseUrl : 'https://petLaudo-fronts-prod-7f97cdd7406c.herokuapp.com'
+    baseUrl : 'http://localhost:8080'
 }
