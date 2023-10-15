ea14149bd07917f9543fcb708100a57022c793f0
joaore10 <joaore1000@gmail.com>
2023-10-15 00:00:43 -0300

    novo style para o header e a sidenav

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  drawer: MatDrawer;
 
   constructor() { }
 
