7c14574902b579d27a7edef36a5e653b30428ddb
joaore10 <joaore1000@gmail.com>
2023-10-15 00:59:56 -0300

    subindo nova config do nav

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -14,7 +14,7 @@ export class ChamadoListComponent implements OnInit {
   ELEMENT_DATA: Chamado[] = [];
   FILTERED_DATA: Chamado[] = [];
   
-  displayedColumns: string[] = ['id', 'titulo', 'cliente','tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  displayedColumns: string[] = ['id', 'nomePaciente','especie','raca','sexo', 'clinica','tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
