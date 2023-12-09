f877e4964aa5b6b600a25ab7cfd53b2e7ede9771
casa-itahy <daniela@casaitahy.com.br>
2023-12-09 10:29:13 -0300

    criação do pdf com o layoult ideal feito

--- src/app/components/pdf/pdf-create/pdf-create.component.ts
+++ src/app/components/pdf/pdf-create/pdf-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { jsPDF } from 'jspdf';
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-pdf-create',
@@ -10,25 +10,27 @@ export class PdfCreateComponent implements OnInit {
 
   //pega o elemento que vai ser transformado em pdf
   @ViewChild("laudoTemplate", { static: true }) laudoTemplate: ElementRef;
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
   createPDF(): void {
-    const DATA = this.laudoTemplate.nativeElement;
-    const pdf: jsPDF = new jsPDF({
-      orientation: 'portrait', // Orientação do documento: 'portrait' ou 'landscape'
-      unit: 'px', // Unidade de medida: 'pt', 'mm', 'cm', 'in'
-      format: 'a4' // Formato do documento: 'a3', 'a4', 'a5', etc.
-    });
-    pdf.html(DATA, {
-      callback: (pdf) => {
-        pdf.save("laudo");
-      },
-      windowWidth:210
-    });
+    const content = this.laudoTemplate.nativeElement;
+
+    const options = {
+      margin: [0, 0, 0, 0],
+      filename: "petLaudo.pdf",
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+      pagebreak: { mode: 'legacy', before: '.page-break' },
+    };
+  
+    html2pdf().set(options).from(content).save();
+
+
   }
+
 
 }
